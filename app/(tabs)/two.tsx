598ba5c9b70277b973b41dca7da60856a5691aaf
priscyd <priscilladunyoh99@gmail.com>
2025-11-25 00:04:48 +0000

    Initial commit
    Generated by create-expo-stack 1.0.3

---
+++ app/(tabs)/two.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function TabTwo() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tab Two</Text>
+    </View>
+  );
+}
